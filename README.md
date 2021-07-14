32a2430c89dc7c47a68e3788481538723aa4c3c3
fuchangling <18226652631@163.com>
2021-07-14 19:57:17 +0800

    fix(优化无限定格式匹配问题):

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # GoSearch
+
 全库全文检索
+
+# 环境
+
+# 运行
+
+``
+chmod 777 search ./search
+``
+
+同级目录创建 config.yml
+
+    # 扫描的文件夹
+    rootdirs:
+    - /root
+    # 正则规则
+    regulars:
+    - description: email
+      expression: ^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$
+    - description: idCard
+      expression: ^\d{15}|\d{18}$
+    - description: ip
+      expression: ^(.*)\d+\.\d+\.\d+\.\d+(.*)\n
+    # 递归扫描
+    search: all
+    # 文件后缀
+    suffix:
+    - txt
--- src/search/main.go
+++ src/search/main.go
@@ -24,23 +24,31 @@ func main() {
 	// 遍历所有目录
 	for _, dir := range c.RootDirs {
 		// 遍历所有类型
-		for _, s := range c.Suffix {
-			files, _ := util.WalkDir(dir, s)
-			if c.Search != "all" {
-				files, _ = util.ListDir(dir, s)
+		if c.Suffix != nil {
+			for _, s := range c.Suffix {
+				lsDir(c, dir, s)
 			}
-			// 遍历所有文件
-			for _, file := range files {
-				// 遍历所有正则规则
-				for _, regular := range c.Regulars {
-					// 匹配正则表达式
-					err := util.ReadLine(file, regular)
-					if err != nil {
-						return
-					}
-				}
+		} else {
+			lsDir(c, dir, "")
+		}
+	}
+	fmt.Println("日志文件保存在：" + "log_" + time.Now().Format("2006-01-02") + ".log")
+}
+
+func lsDir(c config.Config, dir string, suffix string) {
+	files, _ := util.WalkDir(dir, suffix)
+	if c.Search != "all" {
+		files, _ = util.ListDir(dir, suffix)
+	}
+	// 遍历所有文件
+	for _, file := range files {
+		// 遍历所有正则规则
+		for _, regular := range c.Regulars {
+			// 匹配正则表达式
+			err := util.ReadLine(file, regular)
+			if err != nil {
+				return
 			}
 		}
 	}
-	fmt.Println("日志文件保存在：" + "./log_" + time.Now().Format("2006-01-02") + ".log")
 }
